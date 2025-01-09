3f26f8ebf3fc95054e7bffd04e5d5264ff267f95
Satish Kumar <110656011+satish141017@users.noreply.github.com>
2025-01-09 11:37:15 +0530

    dist changed

--- dist/Routes/middleWare/authenticatorMiddleWare.js
+++ dist/Routes/middleWare/authenticatorMiddleWare.js
@@ -17,6 +17,9 @@ function authTokenMiddleware(req, res, next) {
     if (!process.env.JWT_SECRET) {
         throw new Error('JWT_SECRET is not defined in the environment variables.');
     }
+    if (!req.headers['authrization']) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
     const token = req.headers['authrization'].split(' ')[1];
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
