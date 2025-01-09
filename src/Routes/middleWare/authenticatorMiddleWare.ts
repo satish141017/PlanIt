ddea6480ab7727b145099bac856f431d55f8283f
Satish Kumar <110656011+satish141017@users.noreply.github.com>
2025-01-09 11:36:23 +0530

    jsut added

--- src/Routes/middleWare/authenticatorMiddleWare.ts
+++ src/Routes/middleWare/authenticatorMiddleWare.ts
@@ -13,6 +13,9 @@ export function authTokenMiddleware(req:any , res : any , next : any) {
     if (!process.env.JWT_SECRET) {
         throw new Error('JWT_SECRET is not defined in the environment variables.');
     }
+    if(!req.headers['authrization']){
+        return res.status(401).json({ message: 'No token provided' });
+    }
     const token = req.headers['authrization'].split(' ')[1];
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
